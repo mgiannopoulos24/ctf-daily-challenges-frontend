b9183378540e53ecbb092145b2c30d605905db0f
Manos <manospk160@gmail.com>
2024-11-28 19:33:58 +0200

    feat(layout): added layout and basic file structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "../scss/global.scss";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -13,7 +15,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={""}>{children}</body>
+            <body>
+                <Header />
+                <main>{children}</main>
+                <Footer />
+            </body>
         </html>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-export default function Home() {
-    return (
-        <div className="">
-            <main className="">test</main>
-            <footer className=""></footer>
-        </div>
-    );
+import { permanentRedirect } from "next/navigation";
+
+export default async function Index() {
+    permanentRedirect("/home"); // Navigate to the new user profile
 }
